fb17070a9fea0324667c8d078edae973f42cd9a3
chvndler <chvndler.ch@icloud.com>
2022-12-18 22:39:37 -0500

    Update _app.tsx

--- www/src/pages/_app.tsx
+++ www/src/pages/_app.tsx
@@ -6,19 +6,19 @@ import '../styles/reset.css'
 import '../styles/ek-mono.css'
 import '../styles/lateral.css'
 
-class TraxxApp extends App {
-  render() {
-    const {Component, pageProps} = this.props
-    return (
-      <>
-        <Head>
-          <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-        </Head>
-        <Component {...pageProps} />
-      </>
-    )
-  }
+class ArieApp extends App {
+ render() {
+  const {Component, pageProps} = this.props
+  return (
+   <>
+    <Head>
+     <link rel='icon' type='image/x-icon' href='/favicon.ico' />
+     <meta name='viewport' content='width=device-width, initial-scale=1' />
+    </Head>
+    <Component {...pageProps} />
+   </>
+  )
+ }
 }
 
-export default TraxxApp
+export default ArieApp
