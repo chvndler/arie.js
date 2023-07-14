7ebddff2623172a2f2d160045f4be104233dda5c
chvndler <chvndler.ch@icloud.com>
2023-07-13 20:12:37 -0500

    dsd

--- apps/website/src/components/core/arie.cursor.tsx
+++ apps/website/src/components/core/arie.cursor.tsx
@@ -3,9 +3,12 @@
 'use client';
 import React from 'react';
 
-import { ArieCursorPosition } from 'arie-js';
+import useArie from 'arie-js';
 
 export const CursorArie = () => {
+  const {
+    position: { client },
+  } = useArie();
   return (
     <div>
       <ArieCursorPosition />
